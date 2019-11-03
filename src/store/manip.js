90773c6a950bb214d1ff26e42950e62d14e286a3
vencax <vencax@centrum.cz>
2019-11-03 08:41:49 +0100

    refactor

--- src/store/manip.js
+++ src/store/manip.js
@@ -1,4 +1,4 @@
-import {observable, computed, action} from 'mobx'
+import { observable, extendObservable, computed, action } from 'mobx'
 import deepEqual from 'deep-equal'
 
 export default class DataManipStore {
@@ -9,9 +9,12 @@ export default class DataManipStore {
 
   pkName = 'id'
 
+  // all these 3 mothods must be overriden and return Promise
   loadEntry () { throw new Error('implement loadEntry!!') }
   saveEntry () { throw new Error('implement saveEntry!!') }
   initNew () { throw new Error('implement initNew!!') }
+  // this oune shall be overriden, no one wants to alert some unreadables
+  processError (err) { alert(err) }
 
   @action load (id) {
     this.state = 'loading'
@@ -29,6 +32,15 @@ export default class DataManipStore {
     this.record = record
     this.runValidators()
     this.state = 'ready'
+  }
+
+  // called on each update of edit form.
+  // validation performed if got some validators
+  @action updateData (fieldName, value) {
+    value = value === '' ? null : value
+    this.record[fieldName] = value
+    this.runValidators()
+    this.onFieldChange && this.onFieldChange(fieldName, value)
   }
 
   @action runValidators () {
@@ -54,11 +66,11 @@ export default class DataManipStore {
     return ! deepEqual(this.origRecord, this.record, {strict: true})
   }
 
-  isSaveEnabled () {
-    return this.errors.size === 0 && this.isEntityChanged
+  @computed get isSaveEnabled () {
+    return this.errors.size === 0 && this.hasEntityChanged
   }
-  isBeingCreated () {
-    return this.record.has(this.pkName)
+  @computed get isBeingCreated () {
+    return this.record[this.pkName] === null
   }
 
   @action save () {
@@ -70,8 +82,7 @@ export default class DataManipStore {
 
   @action onSaved (saved) {
     this.origRecord = Object.assign({}, saved) // update origRecord coz saved
-    this.record.
-    this.record.merge(saved)
+    extendObservable(this.record, saved)
     this.state = 'ready'
     return this.record
   }
@@ -80,18 +91,4 @@ export default class DataManipStore {
     this.state = 'ready'
     this.processError(err)
   }
-
-  processError (err) {
-    alert(err)  // to be overriden, no one wants to alert some unreadables
-  }
-
-  // called on each update of edit form.
-  // validation performed if got some validators
-  @action updateData (fieldName, value) {
-    value = value === '' ? null : value
-    this.record[fieldName] = value
-    this.runValidators()
-    this.onFieldChange && this.onFieldChange(fieldName, value)
-  }
-
 }
